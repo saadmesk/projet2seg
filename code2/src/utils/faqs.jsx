5b49c79d9f225d35ca7bf624edaeee21ab5ec403
saadmesk <91161917+saadmesk@users.noreply.github.com>
2023-07-23 13:53:31 -0400

    Add files via upload

---
+++ code2/src/utils/faqs.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Importance of pre-approval for a mortgage?",
+    detail:
+      "Getting pre-approved for a mortgage is crucial before you start house hunting. Pre-approval involves a lender evaluating your financial background and creditworthiness to determine the loan amount you qualify for.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "How to find the right neighborhood?",
+    detail:
+      "Finding the right neighborhood involves considering various factors that align with your lifestyle and preferences. Start by listing your priorities, such as proximity to work, schools, public transportation, recreational areas, and amenities.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "What should I look for during a property viewing?",
+    detail:
+      "Property viewings are an excellent opportunity to thoroughly inspect a potential home. Pay attention to the overall condition of the property, including any signs of structural issues, water damage, or wear and tear. Check the functionality of appliances, plumbing, and electrical systems.",
+  },
+];
+export default data;
